88d995b51c5a9627c418ed5bca9b77ed4c1c622e
Arta <artaaghili2002@gmail.com>
2025-01-28 14:07:19 -0800

    projections addition

--- src/routes/ProjectionsPage.jsx
+++ src/routes/ProjectionsPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./CSS/ProjectionsPage.css";
+import { VscDash } from "react-icons/vsc";
 
 function ProjectionsPage() {
   return (
@@ -9,12 +10,40 @@ function ProjectionsPage() {
       </div>
       <div className="line"></div>
       <div className="filter_container">
-          <div className="filter"></div>
-          <div className="filter"></div>
+          <div className="filter2">
+            <div className="filter_header">Customer Name</div>
+            <div className="search-bar-container">Search</div>
+          </div>
+          <div className="filter">
+            <div className="filter_header">Scoop Trend</div>
+            <div className="search-bar-container">Search</div>
+          </div>
+          <div className="filter">
+            <div className="filter_header">Customer Country </div>
+            <div className="search-bar-container">Search</div>
+          </div>
+          <div className="filter">
+            <div className="filter_header">Company Name</div>
+            <div className="search-bar-container">Search</div>
+          </div>
         </div>
-        
+
       <div className="company_area">
-       
+        <div className="company_box">
+          <div className="company_header">Company Name</div>
+          <div className="status_box">
+            <div className="scoop_status">Scoop Trend:</div>
+            <div className="dash">
+              <VscDash className="icon" />
+            </div>
+          </div>
+          <div className="status_box">
+            <div className="accuracy_status">Accuracy:</div>
+          </div>
+        </div>
+        <div className="company_box"></div>
+        <div className="company_box"></div>
+        <div className="company_box"></div>
       </div>
     </div>
   );
