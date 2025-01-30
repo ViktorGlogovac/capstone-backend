93b9ec5e4c6ac3be92a9c780c48ec3b8f9b88040
Arta <artaaghili2002@gmail.com>
2025-01-29 20:25:36 -0800

    filters company done

--- src/routes/CompanyDetails.jsx
+++ src/routes/CompanyDetails.jsx
@@ -7,12 +7,24 @@ function CompanyDetails() {
 
   return (
     <div className="company_content_area">
-      <div className="page-title">{decodeURIComponent(companyName)}</div>
-      <div className="line"></div>
-
-
-
-
+        <div className="page-title">{decodeURIComponent(companyName)}</div>
+        <div className="line"></div>
+        <div className="all_graph_filter_container">
+            <div className="graph_header">Prediction Graph</div>
+            <div className="specifc_graph_filter_container">
+                <div className="graph_filter">
+                    <div className="filter_text">YTD</div>
+                </div>
+                <div className="graph_filter">
+                    <div className="filter_text">Weekly</div>
+                </div>
+                <div className="graph_filter">
+                    <div className="filter_text">Max</div>
+                </div>
+            </div>
+            <div className="graph_container"></div>
+            
+        </div>
     </div>
   );
 }
